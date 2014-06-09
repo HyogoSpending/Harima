d89b6cb78bfe2e963315ad49cb598c670ad7704a
HyogoSpending <HyogoSpending@gmail.com>
2014-06-09 16:43:26 +0900

    up

---
+++ js/setting.js
@@ -0,0 +1,45 @@
+/*
+ * Icons for COFOG taxonomy
+ *
+ */
+var Taxes = Taxes || {};
+
+Taxes.baseKoujo = 330000; // 住民税基礎控除
+Taxes.huyoKoujo = 330000; // 一人分の扶養控除
+Taxes.taxRate = 0.06; // 住民税率
+
+var OpenSpending = OpenSpending || {};
+
+OpenSpending.identifier = 'harimacho_2012_kessan';//平成24年度播磨町決算 
+OpenSpending.year = '2012';
+
+OpenSpending.Styles = OpenSpending.Styles || {};
+
+OpenSpending.Styles.Cofog = {
+
+  /* harima cofog */
+  '20120100': { icon: 'icons/legislative.svg', color: '#531590', bcolor: '#531590' },
+  '20120101': { icon: 'icons/legislative.svg', color: '#531590', bcolor: '#531590' },
+  '20120200': { icon: 'icons/publicaffairs.svg', color: '#531590', bcolor: '#531590' },
+  '20120201': { icon: 'icons/publicaffairs.svg', color: '#531590', bcolor: '#531590' },
+  '20120300': { icon: 'icons/helping-others.svg', color: '#531590', bcolor: '#531590' },
+  '20120301': { icon: 'icons/helping-others.svg', color: '#531590', bcolor: '#531590' },
+  '20120400': { icon: 'icons/health.svg', color: '#531590', bcolor: '#531590' },
+  '20120401': { icon: 'icons/health.svg', color: '#531590', bcolor: '#531590' },
+  '20120500': { icon: 'icons/labour.svg', color: '#531590', bcolor: '#531590' },
+  '20120501': { icon: 'icons/labour.svg', color: '#531590', bcolor: '#531590' },
+  '20120600': { icon: 'icons/farms.svg', color: '#531590', bcolor: '#531590' },
+  '20120601': { icon: 'icons/farms.svg', color: '#531590', bcolor: '#531590' },
+  '20120700': { icon: 'icons/economy-tourism.svg', color: '#531590', bcolor: '#531590' },
+  '20120701': { icon: 'icons/economy-tourism.svg', color: '#531590', bcolor: '#531590' },
+  '20120800': { icon: 'icons/construction.svg', color: '#531590', bcolor: '#531590' },
+  '20120801': { icon: 'icons/construction.svg', color: '#531590', bcolor: '#531590' },
+  '20120900': { icon: 'icons/fire-brigade.svg', color: '#531590', bcolor: '#531590' },
+  '20120901': { icon: 'icons/fire-brigade.svg', color: '#531590', bcolor: '#531590' },
+  '20121000': { icon: 'icons/education.svg', color: '#531590', bcolor: '#531590' },
+  '20121001': { icon: 'icons/education.svg', color: '#531590', bcolor: '#531590' },
+  '20121100': { icon: 'icons/aid-developing-countries.svg', color: '#531590', bcolor: '#531590' },
+  '20121101': { icon: 'icons/aid-developing-countries.svg', color: '#531590', bcolor: '#531590' }
+};
+
+
